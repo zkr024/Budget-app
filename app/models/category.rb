82ca536fb6315495cccf72378873cbf038e01060
zkr024 <zkr.bm.02403@gmail.com>
2022-08-08 14:57:27 -0500

    rubocop

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,6 @@ class Category < ApplicationRecord
   belongs_to :user, foreign_key: :author_id
   has_many :category_purchases
   has_many :purchases, through: :category_purchases
+
+  validates :name, :icon, presence: true
 end
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -2,4 +2,6 @@ class Purchase < ApplicationRecord
   belongs_to :user, foreign_key: :author_id
   has_many :category_purchases
   has_many :categories, through: :category_purchases
+
+  validates :name, :amount, presence: true
 end

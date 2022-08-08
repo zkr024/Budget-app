9549d7670874ccf64b9c85c75c4257f430d1fd2e
zkr024 <zkr.bm.02403@gmail.com>
2022-08-08 14:56:00 -0500

    rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_many :categories, foreign_key: :author_id
   has_many :purchases, foreign_key: :author_id
 
-  validates :first_name, :last_name presence: true
+  validates :first_name, :last_name, presence: true
 end

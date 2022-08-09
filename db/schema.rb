189e21e4651f1cb7550d2c589b9e136cea0c0e76
zkr024 <zkr.bm.02403@gmail.com>
2022-08-08 20:15:26 -0500

    devise set-up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_08_190418) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_09_011012) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,10 +42,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_08_190418) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
+    t.string "full_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "categories", "users", column: "author_id"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-user1 = User.create(first_name: 'Oscar', last_name: 'Bermudez')
+user = User.create(full_name: 'test test', email: 'test@test.com', password: 'password', password_confirmation: 'password')
 
-food = Category.create(author_id: user1.id, name: 'Food', icon: '%')
+food = Category.create(author_id: user.id, name: 'Food', icon: '%')
 
-food.purchases.create(author_id: user1.id, name: 'Sub-way', amount: '5')
+food.purchases.create(author_id: user.id, name: 'Sub-way', amount: '5')

c450ffd78bedb71e0770b5bde4f8e82288699874
zkr024 <zkr.bm.02403@gmail.com>
2022-08-09 11:32:28 -0500

    category page setup

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,9 @@
 class CategoriesController < ApplicationController
-  def index; end
+  def index
+    @categories = Category.where(author_id: current_user)
+  end
+
+  def new
+    @category = Category.new
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :categories, foreign_key: :author_id
   has_many :purchases, foreign_key: :author_id
+
+  validates :full_name, presence: true
 end

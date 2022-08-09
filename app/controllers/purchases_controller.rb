22ea06e08981a2378b9cabdde859905140284022
zkr024 <zkr.bm.02403@gmail.com>
2022-08-09 18:40:12 -0500

    Purchases setup
    index and new page

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,13 +1,29 @@
 class PurchasesController < ApplicationController
   before_action :authenticate_user!
 
-  def index; 
-    @category = Category.find_by_id(params[:category_id])
+  def index
+    @category = Category.where(author_id: current_user, id: params[:category_id])
   end
 
   def new
     @purchase = Purchase.new
     @category = Category.where(author_id: current_user)
-    # @category_purchase = @purchase.category_purchases.build
+  end
+
+  def create
+    purchase = Purchase.new(purchase_params)
+    purchase.author_id = current_user.id
+
+    if purchase.save
+      redirect_to category_purchases_path(params[:category_id])
+    else
+      redirect_to new_category_purchase_path(params[:category_id])
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase).permit(:name, :amount, category_ids: [])
   end
 end
